aa5ea3b12d5cd9433eb97f18c23ad4c8b1e44f5a
Ruoyu <4596611+liangry@users.noreply.github.com>
2023-06-03 17:35:51 +0800

    let config server address configurable by process env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(createProxyMiddleware('/api/v1', {
-    target : 'http://127.0.0.1:8899',
+    target : process.env.CONFIG_SERVER_ADDRESS,
     changeOrigin : true,
     ws: true,
     pathRewrite: {
